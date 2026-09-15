1acd504f9f37920a712c20e936bb0deb5e135709
barakatokikiola <barakattajudeen2018@gmail.com>
2026-09-15 13:34:36 +0100

    jobs page issue fixed

---
+++ src/app/(protected)/jobs/page.tsx
@@ -0,0 +1,8 @@
+export default function JobsPage() {
+  return (
+    <div>
+      <h1>Jobs Page</h1>
+      <p>Welcome to the jobs section. Here you can find various job listings and opportunities.</p>
+    </div>
+  );
+}
